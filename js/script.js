d5d9b862d3f2ef0d9973b3f8639374f292b4f4ad
Laptev S <sl260401@gmail.com>
2023-04-10 16:36:59 +0300

    add gradient

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
-function colorChange() {
-	let hex_numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"];
+let hex_numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"];
 
-	let hex_code = "";
+
+function colorChange() {
+	let hex_code = "#";
 
 	for (let i = 0; i < 6; i++) {
 		let random_index = Math.floor(Math.random() * hex_numbers.length);
@@ -9,12 +10,28 @@ function colorChange() {
 		hex_code += hex_numbers[random_index];
 	}
 
-	document.getElementById("main__hex-code").innerHTML = "#" + hex_code;
-	document.getElementsByTagName("body")[0].style.background = "#" + hex_code;
+	document.getElementById("main__hex-code").innerHTML = "is " + hex_code;
+	document.getElementsByTagName("body")[0].style.background = hex_code;
+}
+
+function gradientChange() {
+	let hex_code_1 = "#";
+	let hex_code_2 = "#";
+
+	for (let i = 0; i < 6; i++) {
+		let random_index_1 = Math.floor(Math.random() * hex_numbers.length);
+		let random_index_2 = Math.floor(Math.random() * hex_numbers.length);
+
+		hex_code_1 += hex_numbers[random_index_1];
+		hex_code_2 += hex_numbers[random_index_2];
+	}
+
+	document.getElementById("main__hex-code").innerHTML = "from " + hex_code_1 + " to " + hex_code_2;
+	document.getElementsByTagName("body")[0].style.background = "linear-gradient(45deg, " + hex_code_1 + ", " + hex_code_2 + ")";
 }
 
 function colorCopy() {
-	let hex_code = document.getElementById("main__hex-code").innerHTML;
+	let hex_code = document.getElementsByTagName("body")[0].style.background;
 
 	let bgColor = "background: " + hex_code + ";";
 	navigator.clipboard.writeText(bgColor);
